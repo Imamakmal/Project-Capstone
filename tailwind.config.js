da8c34218c8f1eda7c512c22599bf27e82df206e
Imam Fauzi Akmal <imamfauziakmal@gmail.com>
2024-12-13 12:54:52 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,21 @@
+import daisyui from 'daisyui';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {
-        fontFamily: {
-          quicksand: ['Quicksand', 'sans-serif'],
-        },
-        colors: {
-          Blue: '#38bdf8',
-          Pink: '#db2777'
-        },
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        quicksand: ['Quicksand', 'sans-serif'],
+      },
+      colors: {
+        Blue: '#38bdf8',
+        Pink: '#db2777'
       },
     },
-    plugins: [
-      require('daisyui'),
-    ],
-  }
-  
+  },
+  plugins: [daisyui],
+};
