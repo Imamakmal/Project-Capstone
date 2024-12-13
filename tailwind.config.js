67f9037d70883bb82c58fbc1457c666a889ee9a4
ImamFA_Register_Github <imamfauziakmal@gmail.com>
2024-12-13 10:33:35 +0700

    Add configuration files and README

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {
+        fontFamily: {
+          quicksand: ['Quicksand', 'sans-serif'],
+        },
+        colors: {
+          Blue: '#38bdf8',
+          Pink: '#db2777'
+        },
+      },
+    },
+    plugins: [
+      require('daisyui'),
+    ],
+  }
+  
